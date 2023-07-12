9164932fa7ce12726b5145e48154cedfa9d09167
жenya <110282058+sttrllxxqq@users.noreply.github.com>
2023-07-12 18:26:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-// 39 - html
+// 39
+const emojis = {
+    '😃': 0,
+    '😊': 0,
+    '🙂': 0,
+    '😐': 0,
+    '😕': 0
+  };
 
+  function updateResults() {
+    const votingResultsDiv = document.getElementById('voting-results');
+    votingResultsDiv.innerHTML = '';
+
+    for (const emoji in emojis) {
+      const count = emojis[emoji];
+      const emojiDiv = document.createElement('div');
+      emojiDiv.innerHTML = `<span class="emoji" onclick="vote('${emoji}')">${emoji}</span> ${count}`;
+      votingResultsDiv.appendChild(emojiDiv);
+    }
+  }
+
+  function vote(emoji) {
+    emojis[emoji]++;
+    updateResults();
+  }
+
+  updateResults();
